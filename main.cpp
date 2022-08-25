352c7effa40ef3734ccb10dc7e4000b4a623608d
Sahu, Rakesh Kumar (BSH) <sahu.rakesh-ext@bshg.com>
2022-08-25 12:15:12 +0530

    added tdd descriptions

--- main.cpp
+++ main.cpp
@@ -5,34 +5,67 @@
 
 using namespace std;
 
-//tdd kata
-TEST_CASE("test set 1") {
-    vector<uint32_t> currentInAmpere {4,5,6,10,11,13};
-    auto rangeReadings = findConsecutiveRanges(currentInAmpere);
-    REQUIRE(rangeReadings.size() == 3);
-    REQUIRE(rangeReadings[0].toString() == "4-6, 3");
-    REQUIRE(rangeReadings[1].toString() == "10-11, 2");
-    REQUIRE(rangeReadings[2].toString() == "13, 1");
+/*****************************************************************************************************************************
+ * Start with the simplest test case of an empty input and move to one and two charging samples
+ * **************************************************************************************************************************/
+TEST_CASE("test set 1 - empty input") {
+    auto rangeReadings = findConsecutiveRanges({});
+    REQUIRE(rangeReadings.size() == 0);
 }
 
-
-TEST_CASE("test set 2") {
-    vector<uint32_t> currentInAmpere {2,3,1};
+TEST_CASE("test set 2 - single charging sample") {
+    vector<uint32_t> currentInAmpere {4};
     auto rangeReadings = findConsecutiveRanges(currentInAmpere);
     REQUIRE(rangeReadings.size() == 1);
-    REQUIRE(rangeReadings[0].toString() == "1-3, 3");
+    REQUIRE(rangeReadings[0].toString() == "4, 1");
 }
 
-TEST_CASE("test set 3") {
+TEST_CASE("test set 3 - Multiple charging samples") {
+    vector<uint32_t> currentInAmpere {4,5,6};
+    auto rangeReadings = findConsecutiveRanges(currentInAmpere);
+    REQUIRE(rangeReadings.size() == 1);
+    REQUIRE(rangeReadings[0].toString() == "4-6, 3");
+}
+
+/*****************************************************************************************************************************
+ * Test for similar charging samples
+ * **************************************************************************************************************************/
+TEST_CASE("test set 4 - duplicate charging samples") {
     vector<uint32_t> currentInAmpere {5,5,5,5,5};
     auto rangeReadings = findConsecutiveRanges(currentInAmpere);
     REQUIRE(rangeReadings.size() == 1);
     REQUIRE(rangeReadings[0].toString() == "5-5, 5");
 }
 
-TEST_CASE("test set 4") {
-    vector<uint32_t> currentInAmpere {2,2,3,4};
+/*****************************************************************************************************************************
+ * Test for irregular charging samples
+ * **************************************************************************************************************************/
+TEST_CASE("test set 5 - irregular charging samples") {
+    vector<uint32_t> currentInAmpere {5,3,4,2,6,9,8};
+    auto rangeReadings = findConsecutiveRanges(currentInAmpere);
+    REQUIRE(rangeReadings.size() == 2);
+    REQUIRE(rangeReadings[0].toString() == "2-6, 5");
+    REQUIRE(rangeReadings[1].toString() == "8-9, 2");
+}
+
+/*****************************************************************************************************************************
+ * Test for decreasing charging samples elements
+ * **************************************************************************************************************************/
+TEST_CASE("test set 6 - Decreasing charging samples") {
+    vector<uint32_t> currentInAmpere {4,3,2,1};
     auto rangeReadings = findConsecutiveRanges(currentInAmpere);
     REQUIRE(rangeReadings.size() == 1);
-    REQUIRE(rangeReadings[0].toString() == "2-4, 4");
+    REQUIRE(rangeReadings[0].toString() == "1-4, 4");
+}
+
+/*****************************************************************************************************************************
+ * Test for Multiple charging samples with multiple ranges
+ * **************************************************************************************************************************/
+TEST_CASE("test set 6 - Multiple charging samples with multiple ranges") {
+    vector<uint32_t> currentInAmpere {4,2,1,3,6,9,7,8,13,11,12};
+    auto rangeReadings = findConsecutiveRanges(currentInAmpere);
+    REQUIRE(rangeReadings.size() == 3);
+    REQUIRE(rangeReadings[0].toString() == "1-4, 4");
+    REQUIRE(rangeReadings[1].toString() == "6-9, 4");
+    REQUIRE(rangeReadings[2].toString() == "11-13, 3");
 }
--- samples.hpp
+++ samples.hpp
@@ -12,6 +12,15 @@ struct RangeData
     std::string toString();
 };
 
+/*****************************************************************************************************************************
+ * 1. capture the range of current measurements - what range of currents are most often encountered while charging.
+ * Input is A set of periodic current samples from a charging session, as an array of integers.
+ * For example: 3, 3, 5, 4, 10, 11, 12
+ * The expected output would be in comma-separated (csv format):
+ * Range, Readings
+ * 3-5, 4
+ * 10-12, 3
+ * **************************************************************************************************************************/
 std::vector<RangeData> findConsecutiveRanges(std::vector<uint32_t> input);
 
 #endif /*__SAMPLES_HPP_ */
